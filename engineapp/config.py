0191f69b4373c885c41b231cb0cab55c41df0f4c
Lakkireddy Sai Pavan <pavan.lucky546@gmail.com>
2018-08-28 21:57:19 +0530

    Update config.py

--- engineapp/config.py
+++ engineapp/config.py
@@ -3,6 +3,6 @@ import os
 
 
 if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-    app_url = 'https://keepanyname.appspot.com'
+    app_url = 'www.workwithflask.appspot.com'
 else:
     app_url = 'http://localhost:8080'
